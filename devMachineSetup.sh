970add208cf563856b39dc8af06fbb5e5d933bde
udayrajMT <udayraj.deshmukh@mindtickle.com>
2020-05-22 01:42:36 +0530

    refactoring around setup

--- devMachineSetup.sh
+++ devMachineSetup.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
-yarn add hotel
-yarn install
+npm install
 
 hotel stop 
 hotel start
 
-networksetup -setautoproxyurl "Wi-Fi" "http://localhost:2000/proxy.pac"
+# networksetup -se"tautoproxyurl "Wi-Fi" "http://localhost:2000/proxy.pac"
 # Turn Wifi Off and On, if not running this script for the first time
 
+hotel rm --name "cheers-bot.mindtickle"
+hotel rm --name "cheer-forwarder"
+hotel rm --name "cheer-client"
+
 #change this to admin.* for admin side features
-hotel add "node start_proxy.js" --name "admin.certificates2" --port 5021 -o proxy.log
-hotel add "npm start" --name "certificates2-ui" --port 5021
+hotel add "node index.js" --name "cheers-bot.mindtickle" --port 5017 -o proxy.log
+hotel add "npm run start" --name "cheer-forwarder" --port 5018
+cd client/ 
+hotel add "npm run start" --name "cheer-client" --port 5019
 
 hotel stop 
 hotel start
